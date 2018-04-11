16476a07a8a088d062248908dc26a2d0e4f2dd32
Davidkrohn <36970676+Davidkrohn@users.noreply.github.com>
2018-04-11 14:35:42 +0200

    v22

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-'use strict';
+// Get dependencies
+var express     = require('express');
+var morgan      = require('morgan');
 
-const express = require('express');
+var app = express();
+var server = require('http').Server(app);
+port = process.env.PORT || 80;
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
+// Catch all other routes and return the index file
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send("hello world!!");
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`)
+// use morgan to log requests to the console
+app.use(morgan('dev')); 
+
+server.listen(port);
+console.log('App running at http://localhost:' + port);
